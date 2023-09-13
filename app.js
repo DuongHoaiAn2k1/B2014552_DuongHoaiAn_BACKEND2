719a3935314ee1eb749e3a5987301723554192b4
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2023-09-13 14:39:50 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactRoute = require("./app/routes/contact.route");
-
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -12,5 +12,13 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactRoute);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
